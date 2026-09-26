70eeb58717ec5d32f8129d3c4b516ab06f3276f1
ANA PAULA DE SOUZA <ana.souza24@univille.br>
2026-09-25 21:58:32 -0300

    Criando variaveis e imprimindo-as

--- function_app.py
+++ function_app.py
@@ -1,25 +1,34 @@
 import azure.functions as func
-import datetime
 import logging
+import os
 
-from http_echo import bp as http_echo_bp
-from http_processa import bp as http_processa_bp
-from timer_chama_outra_function import bp as timer_chama_bp
+# from http_echo import bp as http_echo_bp
+# from http_processa import bp as http_processa_bp
+# from timer_chama_outra_function import bp as timer_chama_bp
 
 app = func.FunctionApp()
 
 
 @app.timer_trigger(
-    schedule="0 */5 * * * *",
+    schedule="0 * * * * *",
     arg_name="myTimer",
     run_on_startup=False,
     use_monitor=False,
 )
 def timer_log_simples(myTimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().isoformat()
-    logging.info(f"[timer_log_simples] Timer trigger executado em: {utc_timestamp}")
+    
+    usuario = os.getenv('USER')
+    database = os.getenv('DATABASE')
+    host = os.getenv('HOST')
+    password = os.getenv('PASSWORD')
+    logging.info(f"Usuário: {usuario}")
+    logging.info(f"Database: {database}")
+    logging.info(f"Host: {host}")
+    logging.info(f"Password: {password}")
 
+# app.register_functions(http_echo_bp)
+# app.register_functions(http_processa_bp)
+# app.register_functions(timer_chama_bp)
 
-app.register_functions(http_echo_bp)
-app.register_functions(http_processa_bp)
-app.register_functions(timer_chama_bp)
+# usuario = 'Ana'
+# logging.info(usuario)
